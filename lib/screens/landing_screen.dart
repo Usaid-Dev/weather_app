ac652248c3838f085cbf66ce8c0ba920559a3c76
usaid <Usaid-Dev>
2022-06-26 18:24:50 +0500

    completed landing screen

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -18,8 +18,55 @@ class landing_Screen extends StatelessWidget {
                         Align(alignment: Alignment.center,
                           child: Image.asset('images/Group 3.png'),
                         ),
-                      ]
-                  )
+                        const Spacer(),
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.pushNamed(context, "/home");
+                          },
+                          child: Container(
+                              height: 72,
+                              margin: const EdgeInsets.only(left: 15,right: 15),
+                              padding: const EdgeInsets.only(left: 20,right: 15),
+                              decoration:
+                              BoxDecoration(
+                                  color: const Color(0xffE0E0E0),
+                                  borderRadius: BorderRadius.circular(15)
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text("Select Current Location",style: TextStyle(fontFamily: "PoppinsMed",fontSize: 20),),
+                                  Image.asset("images/back-arrow.png")
+                                ],
+                              )),
+                        ),
+                        const SizedBox(height: 10,),
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.pushNamed(context, "/selectCity");
+                          },
+                          child: Container(
+                              height: 72,
+                              margin: const EdgeInsets.only(left: 15,right: 15),
+                              padding: const EdgeInsets.only(left: 20,right: 15),
+                              decoration:
+                              BoxDecoration(
+                                  color: const Color(0xff6DC9EF),
+                                  borderRadius: BorderRadius.circular(15)
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text("Select City",style: TextStyle(fontFamily: "PoppinsMed",fontSize: 20,color: Colors.white),
+                                  ),
+                                  Image.asset("images/down_arrow.png")
+                                ],
+                              )
+                          ),
+                        ),
+                        const SizedBox(height: 40,),
+                      ],
+                  ),
               )
             ]
         )
